8f0786d2f95d38b56d014cd5115e62d9334bb243
Julio <juliofornode@gmail.com>
2015-10-21 16:11:32 -0700

    initial commit after adding the project crud module and first edits to schema, views and controller

---
+++ app/models/project.server.model.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Project Schema
+ */
+var ProjectSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please fill Project name',
+		trim: true
+	},
+	description: {
+		type: String,
+		default: '',
+		required: 'Please provide a description',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Project', ProjectSchema);
